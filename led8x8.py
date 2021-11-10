bfa0c7a17736c51c83384acda4f8f6a8fd925e82
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-11-10 21:35:44 +0000

    led

--- led8x8.py
+++ led8x8.py
@@ -17,7 +17,7 @@ class LED8x8(multiprocessing.Process):
 
   def display(self, pattern):
     for i in range(8):
-      self.rowShifter.shiftByte(1<<i) 
+      self.rowShifter.shiftByte(1<<(7-i)) 
       self.colShifter.shiftByte(~(pattern[i]))
       time.sleep(0.002)
       
