7b9ef48f2064a8c866ec1decec069d638208e471
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-11-10 21:23:04 +0000

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 import RPi.GPIO as GPIO
-from led8x8 import LED8X8
+from led8x8 import LED8x8
 
 dataPins = [17, 27]
 latchPins = [5, 6]
@@ -10,7 +10,7 @@ name = "led8x8"
 pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 
 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
-led = LED8X8(dataPins, latchPins, clockPins, name)
+led = LED8x8(dataPins, latchPins, clockPins, name)
 
 try:
   while True:
