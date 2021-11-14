7bcc6a04efc854504bc49804a97304134cc04dde
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-11-14 20:33:53 +0000

    lab6

--- lab6.py
+++ lab6.py
@@ -1,2 +1,39 @@
+import time
+from numpy import random
+import multiprocessing
+import RPi.GPIO as GPIO
+from led8x8 import LED8x8
 
+dataPins = [17, 27]
+latchPins = [5, 6]
+clockPins = [13, 19]
+name = "led8x8"
 
+xpos = 0
+ypos = 0
+boardState = multiprocessing.Array('i', 8)
+boardState[xpos] = 1 << ypos
+led = LED8x8(dataPins, latchPins, clockPins, name, boardState)
+
+led.daemon = True
+led.start()
+
+try:
+  while True:
+    boardState[xpos] = 0
+    xpos = xpos + random.randint(2) - 1
+    ypos = ypos + random.randint(2) - 1
+    if xpos < 0:
+      xpos = 7
+    elif xpos > 7:
+      xpos = 0
+    
+    if ypos < 0:
+      ypos = 7
+    elif ypos > 7:
+      ypos = 0
+    boardState[xpos] = 1 << ypos
+except KeyboardInterrupt:
+  print('exiting random walk')
+finally:
+  led.terminate()
--- led8x8.py
+++ led8x8.py
@@ -5,15 +5,23 @@ from shifter import Shifter
 
 class LED8x8(multiprocessing.Process):
 
-  def __init__(self, data, latch, clock, process_name):
+  def __init__(self, data, latch, clock, process_name, pattern):
     multiprocessing.Process.__init__(self, name=process_name)
+    self.pattern = pattern
     self.rowShifter = Shifter(data[0], latch[0], clock[0])
     self.colShifter = Shifter(data[1], latch[1], clock[1])
 
-  def run(self, pattern):
+  def run(self):
     print("Process started")
-    self.display(pattern)
-    print("Process ended")
+    try:
+      while True:
+        self.display(self.pattern)
+    except KeyboardInterrupt:
+      print('exiting process')
+    finally:  
+      print("Process ended")
+  
+  
 
   def display(self, pattern):
     for i in range(8):
