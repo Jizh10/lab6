321f63fc8e90e00f0281dfde22901f5a77137305
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-11-15 21:01:20 +0000

    test

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ clockPin = 13
 rowShifter = Shifter(dataPins[0], latchPin, clockPin)
 colShifter = Shifter(dataPins[1], latchPin, clockPin)
 
-rowByteVal = 0b10000000
-colByteVal = 0b11111111
+rowByteVal = 0b00000000
+colByteVal = 0b00000000
 
 pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 
 0b10100101, 0b10011001, 0b01000010, 0b00111100]
